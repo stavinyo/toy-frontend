30c2c8ccb94fe1afea51c90c681fb59fde823f2f
Stav Cohen <stavco28@gmail.com>
2023-09-26 21:27:04 +0300

    changed from css to scss

--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -1,21 +1,20 @@
-
 export function HomePage() {
-    return <main>
+    return <main className="home-container">
         <h1>Toyland Extravaganza: Where Imagination Comes to Life</h1>
-        <p>
+        <p className="intro">
             In a world where play knows no bounds, <em>Toyland Extravaganza</em> is your passport to an enchanting realm of joy and wonderment. Dive headfirst into a toy lover's paradise where dreams are forged from plastic, wood, and plush, and imaginations are set free like never before.
         </p>
-        <p>
+        <p className="description">
             Step into a captivating universe of color and creativity, where the aisles are lined with treasures that beckon to children of all ages. From classic favorites to cutting-edge innovations, <em>Toyland Extravaganza</em> is your one-stop destination for an unparalleled toy-shopping experience.
         </p>
-        <p>
+        <p className="features">
             Discover the timeless charm of traditional board games, dolls that come to life, and action figures that spark epic adventures. Dive into a whirlwind of educational wonders that ignite curiosity and spark lifelong learning. Unearth the latest in tech-savvy gadgets, from interactive robots to immersive virtual reality experiences that transport you to distant galaxies.
         </p>
-        <p>
+        <p className="collectors">
             For collectors, our rare and vintage section is a treasure trove of nostalgia, offering a trip down memory lane with cherished toys from yesteryears. And don't forget to explore our sensory-friendly area, where children of all abilities can find toys that cater to their unique needs.
         </p>
-        <p>
+        <p className="adventure">
             At <em>Toyland Extravaganza</em>, we're not just a store; we're a gateway to a world of boundless imagination and endless fun. Come and experience the magic of play as you've never seen it before. Join us today, and let your adventure begin!
         </p>
     </main>
-} 
+}
